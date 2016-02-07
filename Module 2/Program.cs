0be60cde2197dba31d7a4194d83a658af3d92b3a
Surya Raman <ayrusme@gmail.com>
2016-02-07 12:34:20 +0530

    Adding Module 2 of the course to the repository
    Contains the solution to the Chess Board problem.

---
+++ Module 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_edX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=1;i<9;i++)
+            {
+                Console.Write("\n");
+                for(j=1;j<9;j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("X\t");
+                    }
+                    else
+                        Console.Write("O\t");
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
